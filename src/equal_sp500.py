5c348f3d20863148a76c566fb74418ed03cd1480
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-06-01 20:24:27 +0100

    working build created

---
+++ src/equal_sp500.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+import pandas as pd
+import requests
+import requests_cache
+import xlsxwriter 
+import math
+import yfinance as yf
+import datetime as dt
+from dotenv import load_dotenv
+
+session = requests_cache.CachedSession('yfinance.cache')
+session.headers['User-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+
+def fetch_sp500_data():
+    """Fetches current financial market data from Yahoo finance public api"""
+    stocks = pd.read_csv('2023-06-01-sp500.csv')
+    load_dotenv()
+
+    headings = [ 'Ticker', 'Stock Price', 'Market Capitalisation', 'Number of Shares to Buy']
+    result = pd.DataFrame(columns=headings)
+    symbol_groups = list(chunks(stocks['Ticker'], 101))
+    every_str = []
+    for i in range(0, len(symbol_groups)):
+        every_str.append(','.join(symbol_groups[i]))
+    final = pd.DataFrame(columns=headings)
+    for symbol_string in every_str:
+        for stock in symbol_string.split(','):
+            this_stock = yf.Ticker(f"{stock}", session=session)
+            try:
+                final = final.append(
+                    pd.Series(
+                    [
+                        stock,
+                        this_stock.info['currentPrice'],
+                        this_stock.info['marketCap'],
+                        "N/A"
+                    ],
+                    index = headings
+                    ),
+                    ignore_index=True
+            )
+            except(KeyError):
+                pass
+    return final
+
+def convert_sp500_data_to_csv():
+    """Takes current data from the Yahoo finance metrics and returns as a CSV"""
+    final = fetch_sp500_data()
+    today = dt.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    final.to_csv(f'{today}-full_sp500_data.csv')
+
+def chunks(chunk_list, no_of_chunks):
+    """Yield successive n-sized chunks from list"""
+    for i in range(0, len(chunk_list), no_of_chunks):
+        yield chunk_list[i:i + no_of_chunks]
+
+def calculate_no_of_shares_to_purchase():
+    """Uses a rudimentary calculation to set the amount needed to purchase equal weights"""
+    val = None
+    while val == None:
+        portfolio_size = input('Enter the value of your portfolio: ')
+        try:
+            val = float(portfolio_size)
+        except(ValueError):
+            print('Not a number, please enter a legitamate value:')
+        try:
+            today = dt.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+            current_values = pd.read_csv(f'{today}-full_sp500_data.csv')
+        except(FileNotFoundError):
+            current_values = pd.read_csv('2023-06-01-15-54-22-full_sp500_data.csv') 
+            #update above freely
+
+    current_values = current_values.drop('Unnamed: 0', axis=1)
+
+    position_size = val/len(current_values.index)
+    for i in range(0, len(current_values.index)):
+        current_values.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / current_values['Stock Price'][i])
+    return current_values
+
+def output_equal_weights_to_excel():
+    """Creates an xlxs document to be used with the csv data created"""
+    current_values = calculate_no_of_shares_to_purchase()
+    today = dt.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    file_path=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), f'{today}-recommended_trades.xlsx'
+            )
+
+    writer = pd.ExcelWriter(file_path, engine='xlsxwriter')
+    current_values.to_excel(writer, sheet_name='Recommended Trades', index = False)
+
+    background_color = '#0a0a23'
+    font_color = '#ffffff'
+
+    string_format = writer.book.add_format(
+            {
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+
+    dollar_format = writer.book.add_format(
+            {
+                'num_format':'$0.00',
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+
+    integer_format = writer.book.add_format(
+            {
+                'num_format':'0',
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+    column_formats = { 
+                    'A': ['Ticker', string_format],
+                    'B': ['Price', dollar_format],
+                    'C': ['Market Capitalization', dollar_format],
+                    'D': ['Number of Shares to Buy', integer_format]
+                    }
+
+    for column in column_formats.keys():
+        writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+        writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+
+    writer.save()
---
+++ src/wiki_web_scraper.py
@@ -0,0 +1,59 @@
+import datetime as dt
+import pandas as pd
+
+def scrape_sp500_wiki_list():
+    """Fetches the current list of S&P500 companies from wikipedia and stores them as a csv file"""
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    data = pd.read_html(url)
+
+    sp500 = data[0].iloc[:, 0,1,6,7]
+    sp500.columns = ['Ticker', 'Name', 'Date', 'CIK']
+
+    mask = sp500['Date'].str.strip().str.fullmatch('\d{4}-\d{2}-\d{2}')
+    mask.loc[mask.isnull()] = False
+    mask = mask == False
+
+    current = sp500.copy()
+    current.loc[mask, 'Date'] = '1900-01-01'
+    current.loc[:, 'Date'] = pd.to_datetime(current['Date'])
+    current.loc[:, 'CIK'] = current['CIK'].apply(str).str.zfill(10)
+
+    adjustments = data[1]
+
+    columns = ['Date', 'Ticker_Added', 'Name_Added', 'Ticker_Removed', 'Name_Removed', 'Reason']
+    adjustments.columns = columns
+
+    additions = adjustments[~adjustments['Ticker_Added'].isnull()][['Date', 'Ticker_Added', 'Name_Added']]
+    additions.columns = ['Date','Ticker','Name']
+    additions['Action'] = 'Added'
+
+    removals = adjustments[~adjustments['Ticker_Removed'].isnull()][['Date', 'Ticker_Removed', 'Name_Removed']]
+    removals.columns = ['Date','Ticker','Name']
+    removals['Action'] = 'Removed'
+
+    historical = pd.concat([additions, removals])
+
+    missing = current[~current['Ticker'].isin(historical['Ticker'])].copy()
+
+    missing['Action'] = 'Added'
+    missing = missing[['Date', 'Ticker', 'Name', 'Action', 'CIK']]
+
+    sp500_history = pd.concat([historical, missing])
+    sp500_history =sp500_history.sort_values(by=['Date', 'Ticker'], ascending=[False, True])
+
+    today = dt.datetime.today().strftime('%Y-%m-%d')
+
+    current.to_csv(f'{today}-sp500.csv')
+    sp500_history.to_csv(f'{today}-sp500_history.csv')
+
+def scrape_nasdaq100_wiki_list():
+    """Fetches the current list of Nasdaq100 companies from wikipedia and stores them as a csv file"""
+    nasdaq_url = 'https://en.wikipedia.org/w/index.php?title=Nasdaq-100'
+    nasdaq_data = pd.read_html(nasdaq_url)
+
+    nasdaq100 = nasdaq_data[4].iloc[:, [0,1]]
+    nasdaq100.columns = ['Name', 'Ticker']
+        
+    today = dt.datetime.today().strftime('%Y-%m-%d')
+
+    nasdaq100.to_csv(f'{today}-nasdaq100.csv')
